1d071ad191dd4492921d5805cf9b450d03318b32
gujun <gujunchinawuxi@126.com>
2019-10-11 13:41:12 +0800

    2019-10-11

--- config-server/src/main/java/com/gujun/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/gujun/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.gujun.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer //开启配置服务器
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     /*
